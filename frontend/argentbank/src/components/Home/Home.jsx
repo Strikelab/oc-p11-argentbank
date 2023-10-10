3730d779622c8ac71413283458b590237df42538
Strikelab <contact@strikelab.fr>
2023-10-10 14:54:56 +0200

    second commit

--- frontend/argentbank/src/components/Home/Home.jsx
+++ frontend/argentbank/src/components/Home/Home.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-
+import Hero from "../Hero/Hero";
+import Features from "../Features/Features";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <Hero></Hero>
+      <Features></Features>
+    </main>
+  );
 }
