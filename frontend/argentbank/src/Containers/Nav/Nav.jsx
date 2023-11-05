65ffa1a55bc75d87f180bf02574d7429e37ce2a9
Strikelab <contact@strikelab.fr>
2023-11-05 17:50:59 +0100

    last update

--- frontend/argentbank/src/Containers/Nav/Nav.jsx
+++ frontend/argentbank/src/Containers/Nav/Nav.jsx
@@ -5,32 +5,31 @@ import { useLocation } from "react-router-dom";
 import "./nav.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import {logout} from "../../services/authService"
-
+import { logout } from "../../services/authService";
 
 function Nav() {
-  const {userName} = useSelector((state) => state.userProfile);
+  const { userName } = useSelector((state) => state.userProfile);
   const location = useLocation();
   const dispatch = useDispatch();
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+
   const handleLogout = () => {
     logout(dispatch); // Call the logout function from authService
-    navigate("/"); // Redirect to the home page or any other page
+    navigate("/"); // Redirect to the home page
   };
 
   const displayNav = () => {
-    if (location.pathname==="/profile") {
+    if (location.pathname === "/profile") {
       return (
         <>
-        {userName && <span className="main-nav__username">{userName}</span> }
-        <Link onClick={handleLogout} to="/" className="main-nav-item">
-         <i className="fa fa-sign-out"></i>
-          <span>Sign Out</span>
-        </Link>
+          {userName && <span className="main-nav__username">{userName}</span>}
+          <Link onClick={handleLogout} to="/" className="main-nav-item">
+            <i className="fa fa-sign-out"></i>
+            <span>Sign Out</span>
+          </Link>
         </>
       );
-    } else if (location.pathname==="/profile/edit-username") {
+    } else if (location.pathname === "/profile/edit-username") {
       return (
         <div className="main-nav__green">
           <Link to="#" className="main-nav-item">
@@ -52,7 +51,7 @@ function Nav() {
         <span>Sign In</span>
       </Link>
     );
-  }
+  };
 
   return (
     <nav className="main-nav">
